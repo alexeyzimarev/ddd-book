8ae4b0100425540f82bfd7d75ffe0c4b120e3ea8
Alexey Zimarev <alex@zimarev.com>
2018-06-11 23:01:39 +0200

    More to the constructor

--- Marketplace.Domain/ClassifiedAd.cs
+++ Marketplace.Domain/ClassifiedAd.cs
@@ -6,13 +6,23 @@ namespace Marketplace.Domain
     {
         public Guid Id { get; }
 
-        public ClassifiedAd(Guid id)
-        {
-            if (id == default)
-                throw new ArgumentException(nameof(id), "Identity must be specified");
-            
-            Id = id;
-        }
+public ClassifiedAd(Guid id, Guid ownerId)
+{
+    if (id == default)
+        throw new ArgumentException("Identity must be specified", nameof(id));
+    
+    if (ownerId == default)
+        throw new ArgumentException("Owner id must be specified", nameof(ownerId));
+
+    Id = id;
+    _ownerId = ownerId;
+}
+
+        public void SetTitle(string title) => _title = title;
+
+        public void UpdateText(string text) => _text = text;
+
+        public void UpdatePrice(double price) => _price = price;
 
         private Guid _ownerId;
         private string _title;
