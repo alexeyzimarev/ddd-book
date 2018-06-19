0a0dd6325f45ba75913d6591604c3a9b7e154dca
Alexey Zimarev <alex@zimarev.com>
2018-06-19 23:15:56 +0200

    Domain service in a value object

--- Marketplace.Domain/Money.cs
+++ Marketplace.Domain/Money.cs
@@ -5,51 +5,67 @@ namespace Marketplace.Domain
 {
     public class Money : Value<Money>
     {
-        private const string DefaultCurrency = "EUR";
+        public static string DefaultCurrency = "EUR";
+        
+        public static Money FromDecimal(decimal amount, string currency, 
+            ICurrencyLookup currencyLookup) =>
+            new Money(amount, currency, currencyLookup);
 
-        public static Money FromDecimal(decimal amount, string currency = DefaultCurrency) =>
-            new Money(amount, currency);
+        public static Money FromString(string amount, string currency,
+            ICurrencyLookup currencyLookup) =>
+            new Money(decimal.Parse(amount), currency, currencyLookup);
 
-        public static Money FromString(string amount, string currency = DefaultCurrency) =>
-            new Money(decimal.Parse(amount), currency);
-
-        protected Money(decimal amount, string currencyCode = "EUR")
+        protected Money(decimal amount, string currencyCode, ICurrencyLookup currencyLookup)
         {
-            if (decimal.Round(amount, 2) != amount)
+            if (string.IsNullOrEmpty(currencyCode))
+                throw new ArgumentNullException(
+                    nameof(currencyCode), "Currency code must be specified");
+            
+            var currency = currencyLookup.FindCurrency(currencyCode);
+            if (!currency.InUse)
+                throw new ArgumentException($"Currency {currencyCode} is not valid");
+            
+            if (decimal.Round(amount, currency.DecimalPlaces) != amount)
                 throw new ArgumentOutOfRangeException(
                     nameof(amount),
-                    "Amount cannot have more than two decimals");
+                    $"Amount in {currencyCode} cannot have more than {currency.DecimalPlaces} decimals");
 
             _amount = amount;
-            _currencyCode = currencyCode;
+            _currency = currency;
+        }
+
+        private Money(decimal amount, CurrencyDetails currency)
+        {
+            _amount = amount;
+            _currency = currency;
         }
 
         protected readonly decimal _amount;
-        protected readonly string _currencyCode;
+        protected readonly CurrencyDetails _currency;
 
         public Money Add(Money summand)
         {
-            if (_currencyCode != summand._currencyCode)
+            if (_currency != summand._currency)
                 throw new CurrencyMismatchException(
                     "Cannot sum amounts with different currencies");
 
-            return new Money(_amount + summand._amount);
+            return new Money(_amount + summand._amount, _currency);
         }
 
-        public Money Substract(Money subtrahend)
+        public Money Subtract(Money subtrahend)
         {
-            if (_currencyCode != subtrahend._currencyCode)
+            if (_currency != subtrahend._currency)
                 throw new CurrencyMismatchException(
-                    "Cannot substract amounts with different currencies");
+                    "Cannot subtract amounts with different currencies");
 
-            return new Money(_amount - subtrahend._amount);
+            return new Money(_amount - subtrahend._amount, _currency);
         }
 
         public static Money operator +(Money summand1, Money summand2) =>
             summand1.Add(summand2);
 
         public static Money operator -(Money minuend, Money subtrahend) =>
-            minuend.Substract(subtrahend);
+            minuend.Subtract(subtrahend);
     }
 
     public class CurrencyMismatchException : Exception
