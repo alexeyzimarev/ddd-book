ca5e47e85c940a615a108b562c182b8e149e3303
Alexey Zimarev <alex@zimarev.com>
2018-09-06 20:20:28 +0200

    Rolling back some advanced changes in Ch6

--- chapter6/Marketplace.Framework/Entity.cs
+++ chapter6/Marketplace.Framework/Entity.cs
@@ -6,25 +6,14 @@ namespace Marketplace.Framework
 {
     public abstract class Entity<TId> where TId : IEquatable<TId>
     {
-        public TId Id { get; set; }
-
         private readonly List<object> _events;
 
         protected Entity() => _events = new List<object>();
 
-        protected void Apply(object @event)
-        {
-            When(@event);
-            EnsureValidState();
-            _events.Add(@event);
-        }
-
-        protected abstract void When(object @event);
+        protected void Raise(object @event) => _events.Add(@event);
 
         public IEnumerable<object> GetChanges() => _events.AsEnumerable();
 
         public void ClearChanges() => _events.Clear();
-
-        protected abstract void EnsureValidState();
     }
 }
