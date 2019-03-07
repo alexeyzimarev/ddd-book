32abe9482452b205875e651acad69807e4471147
Alexey Zimarev <alex@zimarev.com>
2019-03-07 23:08:20 +0100

    Moving things around

---
+++ chapter13/src/Marketplace/ClientApp/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-app>
+        <v-toolbar app>
+            <v-toolbar-title class="headline text-uppercase">
+                <span>Marketplace</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn
+                    flat
+                    href="https://github.com/vuetifyjs/vuetify/releases/latest"
+                    target="_blank"
+            >
+                <span class="mr-2">Domain-Driven Design</span>
+            </v-btn>
+        </v-toolbar>
+
+        <v-content>
+            <v-container fluid>
+                <router-view></router-view>
+                <HelloWorld></HelloWorld>
+            </v-container>
+        </v-content>
+    </v-app>
+</template>
+
+<script>
+    import HelloWorld from './components/HelloWorld'
+
+    export default {
+        name: 'App',
+        components: {
+            HelloWorld
+        },
+        data() {
+            return {
+                //
+            }
+        }
+    }
+</script>
---
+++ chapter13/src/Marketplace/ClientApp/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import './plugins/vuetify'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
